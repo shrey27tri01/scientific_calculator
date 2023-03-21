cfecbdd6ce6673b6a90b4ddb6c9e89a35299bd6f
shrey27tri01 <shrey.tripathi@iiitb.ac.in>
2023-03-21 14:59:40 +0530

    Added tests for factorial

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -53,4 +53,22 @@ public class CalculatorTest {
         assertNotEquals("Dividing two numbers for False Positive", 69, calculator.div(78.3221, 4), DELTA);
         assertNotEquals("Dividing two numbers for False Positive", 7.99972, calculator.div(9, 1), DELTA);
     }
+
+    @Test
+    public void factorialTruePositive() {
+        assertEquals("Finding factorial of a number for True Positive", 720, calculator.fact(6), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(1), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 6, calculator.fact(3), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 24, calculator.fact(4), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(0), DELTA);
+    }
+
+    @Test
+    public void factorialFalsePositive() {
+        assertNotEquals("Finding factorial of a number for False Positive", 113, calculator.fact(5), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 10, calculator.fact(6), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 42, calculator.fact(4), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 9, calculator.fact(2), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 0, calculator.fact(0), DELTA);
+    }
 }
